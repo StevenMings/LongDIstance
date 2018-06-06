8f41ba4bedd3f5d292dbf6b25057e3511b2d97f6
Steve Mings <stevennmings@gmail.com>
2018-06-05 19:30:07 -0500

    Add project files.

---
+++ LongDIstance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LongDIstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
